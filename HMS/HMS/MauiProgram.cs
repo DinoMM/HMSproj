65bda9e830e48da974570d57b3bea0628d76eeb1
DinoMM <dado2002.tablet@gmail.com>
2024-09-03 02:25:00 +0200

    Pridana Databaza z webu 3.71

--- HMS/HMS/MauiProgram.cs
+++ HMS/HMS/MauiProgram.cs
@@ -55,8 +55,9 @@ namespace HMS
                 usr = "publicUser";
                 tpswd = "TaJnEhEsLo???123456789";
 #endif
-                builder.Services.AddDbContext<DBContext>(opt => opt.UseSqlServer($"Data Source=localhost,1433;Database=MyDatabase;User ID={usr};Password={tpswd};Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False")
-                .EnableSensitiveDataLogging()); //vymazat               //(pomohol som si z internetu tutoriály/AI)
+                builder.Services.AddDbContext<DBContext>(opt => opt.UseSqlServer($"Data Source=localhost,1433;Database=MyDatabase;User ID={usr};Password={tpswd};Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"));               //(pomohol som si z internetu tutoriály/AI)
+
+                builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer($"Data Source=localhost,1433;Database=HlavnaDatabaza;User ID={usr};Password={tpswd};Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"));               //(pomohol som si z internetu tutoriály/AI)
             }
             //pridanie service pre spravu usera a jeho role
             builder.Services.AddIdentity<IdentityUserOwn, IdentityRole>(opt =>
